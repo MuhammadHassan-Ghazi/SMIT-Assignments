8b16d6deaa5a1347cdb42ebec44634c98d5e781e
MuhammadHassan-Ghazi <muhammadhassanyousufghazi@gmail.com>
2025-02-02 14:02:33 +0500

    Reamining sale days

---
+++ Module 2 Assignments/Show Remaining days of Sale/app.js
@@ -0,0 +1,11 @@
+
+function calculateDaysLeft() {
+    var current = new Date().getTime();
+    var saleEnd = new Date("14 Feb 2025 00:00:00").getTime();
+
+    var res = (saleEnd - current) / (1000 * 60 * 60 * 24);
+    res = Math.ceil(res); 
+    return res;
+}
+
+document.getElementById('remDays').innerHTML = calculateDaysLeft();
